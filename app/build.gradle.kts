5744f25cc3efbea749bacefabfcc91088a044f7c
YashBhadange2006 <yash271106@gmail.com>
2026-07-17 16:09:49 +0530

    Added Permission Screen UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.navigation.fragment)
     implementation(libs.androidx.navigation.ui)
+    implementation("com.google.android.material:material:1.12.0")
     implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
